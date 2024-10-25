db1808bf88efb206bf5501a41dcc2c7b919c740e
Manuel Campoliete <manuelcampoliete@gmail.com>
2024-10-25 12:20:57 -0300

    add gdb screenshots to explain context switch
    Co-Authored-By: Valentín Bersi <valentinbersi@gmail.com>
    Co-Authored-By: Tomás Hevia <118986985+tomashevia27@users.noreply.github.com>
    Co-Authored-By: Camilo Sassone Irrazabal <40524371+Vityyy@users.noreply.github.com>

--- sched/kern/sched.c
+++ sched/kern/sched.c
@@ -13,168 +13,171 @@ void sched_halt(void);
 int
 get_curenv_idx()
 {
-    int i = 0;
-    while (i < NENV) {
-        if (&envs[i] == curenv)
-            break;
-        ++i;
-    }
-    return i;
+	int i = 0;
+	while (i < NENV) {
+		if (&envs[i] == curenv)
+			break;
+		++i;
+	}
+	return i;
 }
 #endif
 
 // Choose a user environment to run and run it.
 void
-sched_yield(void) {
+sched_yield(void)
+{
 #ifdef SCHED_ROUND_ROBIN
-    // Implement simple round-robin scheduling.
-    //
-    // Search through 'envs' for an ENV_RUNNABLE environment in
-    // circular fashion starting just after the env this CPU was
-    // last running. Switch to the first such environment found.
-    //
-    // If no envs are runnable, but the environment previously
-    // running on this CPU is still ENV_RUNNING, it's okay to
-    // choose that environment.
-    //
-    // Never choose an environment that's currently running on
-    // another CPU (env_status == ENV_RUNNING). If there are
-    // no runnable environments, simply drop through to the code
-    // below to halt the cpu.
+	// Implement simple round-robin scheduling.
+	//
+	// Search through 'envs' for an ENV_RUNNABLE environment in
+	// circular fashion starting just after the env this CPU was
+	// last running. Switch to the first such environment found.
+	//
+	// If no envs are runnable, but the environment previously
+	// running on this CPU is still ENV_RUNNING, it's okay to
+	// choose that environment.
+	//
+	// Never choose an environment that's currently running on
+	// another CPU (env_status == ENV_RUNNING). If there are
+	// no runnable environments, simply drop through to the code
+	// below to halt the cpu.
 
-    // Your code here - Round robin
+	// Your code here - Round robin
 
-    int curenv_idx = -1;
-    int i = 0;
-    struct Env *chosen = NULL;
-    bool found = false;
+	int curenv_idx = -1;
+	int i = 0;
+	struct Env *chosen = NULL;
+	bool found = false;
 
-    if (curenv) {
-        curenv_idx = get_curenv_idx();
-        if (curenv_idx != NENV - 1)
-            i = curenv_idx + 1;
-        else
-            curenv_idx = -1;  // to avoid looping twice
-    }
+	if (curenv) {
+		curenv_idx = get_curenv_idx();
+		if (curenv_idx != NENV - 1)
+			i = curenv_idx + 1;
+		else
+			curenv_idx = -1;  // to avoid looping twice
+	}
 
-    while (i < NENV && !found) {
-        if (envs[i].env_status == ENV_RUNNABLE) {
-            chosen = &envs[i];
-            found = true;
-        }
-        ++i;
-    }
+	while (i < NENV && !found) {
+		if (envs[i].env_status == ENV_RUNNABLE) {
+			chosen = &envs[i];
+			found = true;
+		}
+		++i;
+	}
 
-    i = 0;
-    while (i < curenv_idx && !found) {
-        if (envs[i].env_status == ENV_RUNNABLE) {
-            chosen = &envs[i];
-            found = true;
-        }
-        ++i;
-    }
+	i = 0;
+	while (i < curenv_idx && !found) {
+		if (envs[i].env_status == ENV_RUNNABLE) {
+			chosen = &envs[i];
+			found = true;
+		}
+		++i;
+	}
 
-    if (!found && curenv && curenv->env_status == ENV_RUNNING)
-        chosen = curenv;
+	if (!found && curenv && curenv->env_status == ENV_RUNNING)
+		chosen = curenv;
 
 #endif
-    
+
 #ifdef SCHED_PRIORITIES
-    // Implement simple priorities scheduling.
-    //
-    // Environments now have a "priority" so it must be consider
-    // when the selection is performed.
-    //
-    // Be careful to not fall in "starvation" such that only one
-    // environment is selected and run every time.
+	// Implement simple priorities scheduling.
+	//
+	// Environments now have a "priority" so it must be consider
+	// when the selection is performed.
+	//
+	// Be careful to not fall in "starvation" such that only one
+	// environment is selected and run every time.
 
-    // Your code here - Priorities
+	// Your code here - Priorities
 
-    static uint32_t cicle_count = 0;
+	static uint32_t cicle_count = 0;
 
-    if (cicle_count % 250 == 0)
-        for (int i = 0; i < NENV; i++)
-            envs[i].env_priority = ENV_DEFAULT_PRIORITY;
+	if (cicle_count % 250 == 0)
+		for (int i = 0; i < NENV; i++)
+			envs[i].env_priority = ENV_DEFAULT_PRIORITY;
 
-    // If the interruption was a timer interruption, we decrement thve curenv's priority by 1
-    // (if it is runnable and its priority is not the lowest)
-    uint32_t curenv_priority = 0;
-    if (curenv && curenv->env_tf.tf_trapno == IRQ_OFFSET + IRQ_TIMER) {
-        if (curenv->env_status == ENV_RUNNABLE && curenv->env_priority != 1)
-            curenv->env_priority = curenv->env_priority - 1;
-    }
+	// If the interruption was a timer interruption, we decrement thve curenv's priority by 1
+	// (if it is runnable and its priority is not the lowest)
+	uint32_t curenv_priority = 0;
+	if (curenv && curenv->env_tf.tf_trapno == IRQ_OFFSET + IRQ_TIMER) {
+		if (curenv->env_status == ENV_RUNNABLE && curenv->env_priority != 1)
+			curenv->env_priority = curenv->env_priority - 1;
+	}
 
-    struct Env *chosen = NULL;
-    uint32_t highest_priority = 0;
+	struct Env *chosen = NULL;
+	uint32_t highest_priority = 0;
 
-    for (int i = 0; i < NENV; i++) {
-        if (envs[i].env_status == ENV_RUNNABLE && envs[i].env_priority > highest_priority) {
-            highest_priority = envs[i].env_priority;
-            chosen = &envs[i];
-        }
-    }
+	for (int i = 0; i < NENV; i++) {
+		if (envs[i].env_status == ENV_RUNNABLE &&
+		    envs[i].env_priority > highest_priority) {
+			highest_priority = envs[i].env_priority;
+			chosen = &envs[i];
+		}
+	}
 
-    ++cicle_count;
+	++cicle_count;
 
 #endif
 
-    // Without scheduler, keep runing the last environment while it exists
-    // if (curenv) {
-    // 	env_run(curenv);
-    // }
+	// Without scheduler, keep runing the last environment while it exists
+	// if (curenv) {
+	// 	env_run(curenv);
+	// }
 
-    if (chosen)
-        env_run(chosen); // never returns
+	if (chosen)
+		env_run(chosen);  // never returns
 
-    // sched_halt never returns
-    sched_halt();
+	// sched_halt never returns
+	sched_halt();
 }
 
 // Halt this CPU when there is nothing to do. Wait until the
 // timer interrupt wakes it up. This function never returns.
 //
 void
-sched_halt(void) {
-    int i;
+sched_halt(void)
+{
+	int i;
 
-    // For debugging and testing purposes, if there are no runnable
-    // environments in the system, then drop into the kernel monitor.
-    for (i = 0; i < NENV; i++) {
-        if ((envs[i].env_status == ENV_RUNNABLE ||
-             envs[i].env_status == ENV_RUNNING ||
-             envs[i].env_status == ENV_DYING))
-            break;
-    }
-    if (i == NENV) {
-        cprintf("No runnable environments in the system!\n");
-        while (1)
-            monitor(NULL);
-    }
+	// For debugging and testing purposes, if there are no runnable
+	// environments in the system, then drop into the kernel monitor.
+	for (i = 0; i < NENV; i++) {
+		if ((envs[i].env_status == ENV_RUNNABLE ||
+		     envs[i].env_status == ENV_RUNNING ||
+		     envs[i].env_status == ENV_DYING))
+			break;
+	}
+	if (i == NENV) {
+		cprintf("No runnable environments in the system!\n");
+		while (1)
+			monitor(NULL);
+	}
 
-    // Mark that no environment is running on this CPU
-    curenv = NULL;
-    lcr3(PADDR(kern_pgdir));
+	// Mark that no environment is running on this CPU
+	curenv = NULL;
+	lcr3(PADDR(kern_pgdir));
 
-    // Mark that this CPU is in the HALT state, so that when
-    // timer interupts come in, we know we should re-acquire the
-    // big kernel lock
-    xchg(&thiscpu->cpu_status, CPU_HALTED);
+	// Mark that this CPU is in the HALT state, so that when
+	// timer interupts come in, we know we should re-acquire the
+	// big kernel lock
+	xchg(&thiscpu->cpu_status, CPU_HALTED);
 
-    // Release the big kernel lock as if we were "leaving" the kernel
-    unlock_kernel();
+	// Release the big kernel lock as if we were "leaving" the kernel
+	unlock_kernel();
 
-    // Once the scheduler has finishied it's work, print statistics on
-    // performance. Your code here
+	// Once the scheduler has finishied it's work, print statistics on
+	// performance. Your code here
 
-    // Reset stack pointer, enable interrupts and then halt.
-    asm volatile("movl $0, %%ebp\n"
-                 "movl %0, %%esp\n"
-                 "pushl $0\n"
-                 "pushl $0\n"
-                 "sti\n"
-                 "1:\n"
-                 "hlt\n"
-                 "jmp 1b\n"
-            :
-            : "a"(thiscpu->cpu_ts.ts_esp0));
+	// Reset stack pointer, enable interrupts and then halt.
+	asm volatile("movl $0, %%ebp\n"
+	             "movl %0, %%esp\n"
+	             "pushl $0\n"
+	             "pushl $0\n"
+	             "sti\n"
+	             "1:\n"
+	             "hlt\n"
+	             "jmp 1b\n"
+	             :
+	             : "a"(thiscpu->cpu_ts.ts_esp0));
 }
--- sched/sched.md
+++ sched/sched.md
@@ -1,3 +1,39 @@
 # sched
 
-Lugar para respuestas en prosa, seguimientos con GDB y documentación del TP.
+El estado del stack al inicio de la llamada a context_switch. La primera dirección es la return address y la segunda la dirección al trapframe:
+![](img/gdb1.png)
+
+Ejecutamos la 'add', omitiendo la return address. Ahora tenemos en el tope del stack a la dirección del trapframe:
+![](img/gdb2.png)
+
+Si inspeccionamos esa dirección, vemos que efectivamente es el trapframe:
+![](img/gdb3.png)
+
+Ejecutamos el 'pop', y ahora el stack es el trapframe. Verificamos que el contenido coincide con el contenido previamente inspeccionado:
+![](img/gdb4.png)
+
+Ejecutamos 'popa' y se restauran los registros de uso general:
+![](img/gdb5.png)
+
+Ejecutamos el siguiente 'pop' y se restaura `es`:
+![](img/gdb6.png)
+
+Ejecutamos el siguiente 'pop' y se restaura `ds`:
+![](img/gdb7.png)
+
+Ejecutamos la 'add' y nos salteamos los campos trapno y err:
+![](img/gdb8.png)
+
+Finalmente se ejecuta 'iret' y se restauran los registros especiales:
+![](img/gdb9.png)
+
+De aquí en adelante se ejecutan instrucciones en espacio de usuario
+- `eip` apunta a la instrucción sobre la cual se debe reanudar la ejecución del programa de usuario
+- `cs` indicaría que estamos en ring 3 (*)
+- `eflags` recupera el estado de la ALU del programa de usuario
+- `esp` apunta al stack del usuario
+
+(*)
+`cs` = 0000001b [16] = 0000 0000 0000 0000 0000 0000 0001 10**11** [2]
+
+---
